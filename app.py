98fc50139cf1903a8b6d36321a53a76ad6ba3aff
aanch-343 <aanchalnshetty111@gmail.com>
2024-03-12 15:18:58 +0530

    update methods

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/receive-image',methods=['POST'])
+@app.route('/receive-image', methods=['GET', 'POST'])
 def receive_image():
     try:
         # Receive data from the request
