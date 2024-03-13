b060f6412a352d2347fd1588b7bfa2c9bd2a699d
aanch-343 <aanchalnshetty111@gmail.com>
2024-03-13 12:12:48 +0530

    added base 64

--- app.py
+++ app.py
@@ -30,24 +30,26 @@
 #     app.run(debug=True)  # Run the Flask app
 
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from PIL import Image
 import io
+import base64  # Import base64 module
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
 @app.route('/receive-image', methods=['POST'])
 def receive_image():
     try:
-        # Extract image buffer, image name, question, and answer key from request body
-        image_data = request.json['image']
+        # Extract image data, image name, question, and answer key from request JSON
+        image_data_base64 = request.json['image']
         image_name = request.json['imageName']
         question = request.json['question']
         answer_key = request.json['answerkey']
 
-        # Convert image buffer to image
-        image = Image.open(io.BytesIO(bytearray(image_data)))
+        # Decode base64-encoded image data
+        image_data = io.BytesIO(base64.b64decode(image_data_base64))
+
+        # Open image using PIL
+        image = Image.open(image_data)
 
         # Save the image as JPEG file
         image.save(image_name + '.jpg')
@@ -56,14 +58,12 @@ def receive_image():
         print("Image:", image_name)
         print("Question:", question)
         print("Answer Key:", answer_key)
-        print("Image Buffer:", image_data)
 
-        # Return a response acknowledging the receipt of data along with the question and answer key
+        # Return a response acknowledging the receipt of data
         return jsonify(message="Data received and logged"), 200
     except Exception as e:
         print("Error receiving data:", str(e))
         return jsonify(error="Error receiving data"), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the Flask app
-
+    app.run(debug=True)
